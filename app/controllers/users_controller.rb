8dbff9c71e0256f08a1cc9e931c01b2bb14771f4
Alexandre Daoud <alex.daoud@mac.com>
2013-09-18 04:10:47 +0100

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action(:signed_in_user,only:[:edit, :update])
+  before_action(:correct_user, only:[:edit, :update])
 	def new
 		@user=User.new
 	end
@@ -14,8 +16,20 @@ class UsersController < ApplicationController
   		flash[:success] = "Welcome to the Sample App"
   		redirect_to @user
   	else
-  		render 'new'
+  		render('new')
   	end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to(@user)
+    else
+      render('edit')
+    end
   end
 
 	private
@@ -23,4 +37,16 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to(signin_url, notice: "Please sign in") unless signed_in?
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
