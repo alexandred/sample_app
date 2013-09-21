1911676612b969021aed8173f489829498ac69bd
Alexandre Daoud <alex.daoud@mac.com>
2013-09-21 05:06:11 +0100

    Finish user edit etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  before_action(:signed_in_user,only:[:edit, :update])
+  before_action(:signed_in_user,only:[:index, :edit, :update, :destroy])
   before_action(:correct_user, only:[:edit, :update])
+  before_action(:admin_user, only: :destroy)
 	def new
 		@user=User.new
 	end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
 	def show
   	@user=User.find(params[:id])
@@ -32,6 +37,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to(users_url)
+  end
+
 	private
 
 	def user_params
@@ -49,4 +60,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
+  def admin_user
+    redirect_to(root_url) unless curent_user.admin?
+  end
+  
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,9 +19,9 @@ describe "Authentication" do
 			it { should have_title('Sign in') }
 			it { should have_selector('div.alert.alert-error', text: 'Invalid')}
 			describe "after visiting another page" do
-        		before { click_link "Home" }
-        		it { should_not have_selector('div.alert.alert-error') }
-      		end
+				before { click_link "Home" }
+				it { should_not have_selector('div.alert.alert-error') }
+			end
 		end
 
 		describe "with valid information" do
@@ -33,7 +33,9 @@ describe "Authentication" do
 			end
 
 			it {should have_title(user.name)}
+			it {should have_link('Users', href:users_path)}
 			it {should have_link('Profile', href: user_path(user))}
+			it {should have_link('Settings', href: edit_user_path(user))}
 			it {should have_link('Sign out', href: signout_path)}
 			it {should_not have_link('Sign in', href:signin_path)}
 
@@ -83,6 +85,11 @@ describe "Authentication" do
 					it { should have_title('Sign in')}
 				end
 
+				describe "attempting to access index" do
+					before { visit users_path }
+					it { should have_title("Sign in") }
+				end
+
 				describe "submitting to the update action" do
 					before { patch user_path(user) }
 					specify { expect(response).to redirect_to(signin_path)}
@@ -105,7 +112,6 @@ describe "Authentication" do
 				before { patch user_path(wrong_user)}
 				specify { expect(response).to redirect_to(root_url)}
 			end
-		end	
-	end			
+		end			
+	end
 end
-
